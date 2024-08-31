03dc0f8a104dc0cffbd654613fe6d7348d68f0cb
tybikkryt <michaarmavir@gmail.com>
2024-08-31 23:09:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -226,12 +226,15 @@ echo -e "${green}Running...${plain}"
 install_base
 install_x-ui $1
 
-apt-get install sqlite3 openssl
+apt-get install sqlite3 openssl jq -y
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/public.key -days 3650 -subj "/CN=APP"
 next_id=$(($(sqlite3 /etc/x-ui/x-ui.db "SELECT IFNULL(MAX(id), 0) FROM settings;") + 1))
 second_id=$((next_id + 1))
 sqlite3 /etc/x-ui/x-ui.db "INSERT INTO settings VALUES (${next_id}, 'webKeyFile', '/etc/ssl/private/private.key'); INSERT INTO settings VALUES (${second_id}, 'webCertFile', '/etc/ssl/certs/public.key');"
 x-ui restart
-echo -e "${green}URL: https://$(hostname -i):2053$(sqlite3 /etc/x-ui/x-ui.db 'SELECT value FROM settings WHERE key="webBasePath"')${plain}"
-echo -e "${green}Username: $(sqlite3 /etc/x-ui/x-ui.db 'SELECT username FROM users')${plain}"
-echo -e "${green}Password: $(sqlite3 /etc/x-ui/x-ui.db 'SELECT password FROM users')${plain}"
+username=$(sqlite3 /etc/x-ui/x-ui.db 'SELECT username FROM users')
+password=$(sqlite3 /etc/x-ui/x-ui.db 'SELECT password FROM users')
+webBasePath=$(sqlite3 /etc/x-ui/x-ui.db 'SELECT value FROM settings WHERE key="webBasePath"')
+echo -e "${green}URL: https://$(hostname -i):2053${webBasePath}${plain}"
+echo -e "${green}Username: ${username}${plain}"
+echo -e "${green}Password: ${password}${plain}"
