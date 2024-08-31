4c4b73e698685a56d9ca88899e5449d77a011c21
tybikkryt <michaarmavir@gmail.com>
2024-08-31 12:47:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -228,7 +228,7 @@ install_x-ui $1
 
 apt-get install sqlite3 openssl
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/public.key -days 3650 -subj "/CN=APP"
-sqlite3 /etc/x-ui/x-ui.db "INSERT INTO settings VALUES (%d, 'webKeyFile', '/etc/ssl/private/private.key'); INSERT INTO settings VALUES (%d, 'webCertFile', '/etc/ssl/certs/public.key');"
+sqlite3 /etc/x-ui/x-ui.db "INSERT INTO settings VALUES ($(sqlite3 /etc/x-ui/x-ui.db "SELECT IFNULL(MAX(id), 0) FROM settings"), 'webKeyFile', '/etc/ssl/private/private.key'); INSERT INTO settings VALUES ($(sqlite3 /etc/x-ui/x-ui.db "SELECT IFNULL(MAX(id), 0) FROM settings"), 'webCertFile', '/etc/ssl/certs/public.key');"
 x-ui restart
 echo -e "${green}URL: https://$(hostname -i):2053$(sqlite3 /etc/x-ui/x-ui.db 'SELECT value FROM settings WHERE key="webBasePath"')${plain}"
 echo -e "${green}Username: $(sqlite3 /etc/x-ui/x-ui.db 'SELECT username FROM users')${plain}"
