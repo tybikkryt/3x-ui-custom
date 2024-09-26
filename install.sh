29d452b2ca2414dfe628226f94d2e453a1d6a520
tybikkryt <michaarmavir@gmail.com>
2024-09-26 22:48:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -258,13 +258,13 @@ mkdir /root/api
 
 cat << 'EOF' | sudo tee /root/api/getClient > /dev/null
 #!/bin/bash
+echo "Content-type: application/json"
+echo ""
+email=$(echo "$QUERY_STRING" | sed -n 's/^.*email=\([^&]*\).*$/\1/p')
+timestamp=$(echo "$QUERY_STRING" | sed -n 's/^.*timestamp=\([^&]*\).*$/\1/p')
 password=$(echo "$QUERY_STRING" | sed -n 's/^.*password=\([^&]*\).*$/\1/p')
 passwordHash=$(echo -n "$password" | sha256sum | awk '{print $1}')
 if [ ${passwordHash} == "e597e26004b4ea341695dd9e2cc5ce301d01fccdcdc066b513b60db46431cc43" ]; then
-    echo "Content-type: application/json"
-    echo ""
-    email=$(echo "$QUERY_STRING" | sed -n 's/^.*email=\([^&]*\).*$/\1/p')
-    timestamp=$(echo "$QUERY_STRING" | sed -n 's/^.*timestamp=\([^&]*\).*$/\1/p')
     if [ $(sqlite3 /etc/x-ui/x-ui.db "SELECT COUNT(*) FROM client_traffics WHERE email = '${email}'") == 1 ]; then
         uuid=$(sqlite3 /etc/x-ui/x-ui.db "SELECT settings FROM inbounds WHERE id = 1" | jq -r --arg var "$email" '.clients[] | select(.email == $var) | .id')
     else
@@ -273,13 +273,18 @@ if [ ${passwordHash} == "e597e26004b4ea341695dd9e2cc5ce301d01fccdcdc066b513b60db
         curl -k -b cookie -c cookie "https://localhost:2053$(cat /root/webBasePath)panel/inbound/addClient" -X "POST" -d "id=1&settings=%7B%22clients%22%3A%20%5B%7B%0A%20%20%22id%22%3A%20%22${uuid}%22%2C%0A%20%20%22flow%22%3A%20%22xtls-rprx-vision%22%2C%0A%20%20%22email%22%3A%20%22${email}%22%2C%0A%20%20%22limitIp%22%3A%200%2C%0A%20%20%22totalGB%22%3A%200%2C%0A%20%20%22expiryTime%22%3A%20${timestamp}%2C%0A%20%20%22enable%22%3A%20true%2C%0A%20%20%22tgId%22%3A%20%22%22%2C%0A%20%20%22subId%22%3A%20%22$(randomSubId)%22%2C%0A%20%20%22reset%22%3A%200%0A%7D%5D%7D" > addClient.txt
     fi
     config=$(echo "vless://${uuid}@$(hostname -I | awk '{print $1}'):443?type=tcp&security=reality&pbk=$(cat /root/publicKey)&fp=random&sni=google.com&sid=$(cat /root/sid0)&spx=%2F&flow=xtls-rprx-vision#$(cat /root/remark)" | base64)
-	cat <<EOL
-		{"status": "success", "config": "${config}"}
-	EOL
 else
-	echo "Status: 404 Not Found"
-	echo ""
+    status="bruh"
 fi
+if [ ${email} == "" ]; then
+	status="lol"
+fi
+if [ ${timestamp} == "" ]; then
+	status="jk"
+fi
+cat <<EOL
+{"status": "${status}", "config": "${config}"}
+EOL
 EOF
 
 cat << 'EOF' | sudo tee /usr/bin/randomUUID > /dev/null
